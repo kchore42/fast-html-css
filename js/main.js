33dc38469c48ad593ee5b82850f1e97a054ec5f7
김현진 <kchore615@gmail.com>
2024-05-07 05:28:29 +0900

    함수 - 화살표 함수

--- js/main.js
+++ js/main.js
@@ -1,25 +1,15 @@
-function getGrade(score) {
-    if (score === 100) {
-      return 'A+';
-    } else if (score >= 90) {
-      return 'A';
-    } else if (score === 89) {
-      return 'B+';
-    } else if (score >= 80) {
-      return 'B';
-    } else if (score === 79) {
-      return 'C+';
-    } else if (score >= 70) {
-      return 'C';
-    } else if (score === 69) {
-      return 'D+';
-    } else if (score >= 60) {
-      return 'D';
-    } else {
-      return 'F';
-    }
-  }
-  
-  const grade = getGrade(90);
-  console.log(grade);
-  grade 가 90 일 때에는 결과가 A
+화살표 함수
+함수를 선언하는 방식 중 또 다른 방법은 화살표 함수 문법을 사용 하는 것 입니다.
+
+const add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(1, 2));
+
+function 키워드 대신에 => 문자를 사용해서 함수를 구현했는데요, 화살표의 좌측에는 함수의 파라미터, 화살표의 우측에는 코드 블록이 들어옵니다.
+
+그런데, 만약에 위와 같이 코드 블록 내부에서 바로 return 을 하는 경우는 다음과 같이 줄여서 쓸 수도 있습니다.
+
+const add = (a, b) => a + b;
+console.log(add(1, 2));
