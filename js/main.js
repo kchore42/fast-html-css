bf42f7e8143ddd400dd27bf0c3c0c833ef13dece
김현진 <kchore615@gmail.com>
2024-05-07 03:53:21 +0900

    자바 - 변수

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-const myName = document.getElementById('my-name');
-console.log(myName.innerText);
-const myName2 = 1;
-let myName3 = 3;
-myName3 = 4;
-console.log('Hello JavaScript');
+
 console.log('Hello JavaScript');
 let value = 1;
 console. log(value);
-또 다시 선언 못함
+
+상수의 특징 한 번 설정하면 바꾸지 못 함
+
+const a = = 1;
+
+var a = 1; 
+권장은 let
+
+let text = 'Hello' 
+let text = "자바스크립트";
+
+아무거나 사용해도 됨 '', "", ;도 마찬가지
+
+let good = true;
+let loading =false;
+
+let good = null;
+let something = undefined;
+
