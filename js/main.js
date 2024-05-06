45ed6c5119a5c1326b3fd4117c6c8f65c2d1c213
김현진 <kchore615@gmail.com>
2024-05-07 06:01:29 +0900

    객체 안에 함수 넣기

--- js/main.js
+++ js/main.js
@@ -1,39 +1,44 @@
-객체 비구조화 할당
-print 함수를 보시면 파라미터로 받아온 hero 내부의 값을 조회 할 때 마다 hero. 를 입력하고 있는데, 객체 비구조화 할당이라는 문법을 사용하면 코드를 더욱 짧고 보기 좋게 작성 할 수 있습니다.
+객체 안에 함수 넣기
+객체 안에 함수를 넣을 수도 있습니다. 
 
-이 문법은 "객체 구조 분해" 라고 불리기도 합니다.
-  
+const dog = {
+  name: '멍멍이',
+  sound: '멍멍!',
+  say: function say() {
+    console.log(this.sound);
+  }
+};
 
-print 함수를 보시면 파라미터로 받아온 hero 내부의 값을 조회 할 때 마다 hero. 를 입력하고 있는데, 객체 비구조화 할당이라는 문법을 사용하면 코드를 더욱 짧고 보기 좋게 작성 할 수 있습니다.
+dog.say();
+결과는 다음과 같습니다.
 
+멍멍!
+함수가 객체안에 들어가게 되면, this 는 자신이 속해있는 객체를 가르키게 됩니다.
 
+함수를 선언 할 때에는 이름이 없어도 됩니다.
 
-const ironMan = {
-    name: '토니 스타크',
-    actor: '로버트 다우니 주니어',
-    alias: '아이언맨'
+const dog = {
+  name: '멍멍이',
+  sound: '멍멍!',
+  say: function() {
+    console.log(this.sound);
+  }
+};
+
+const cay{
+    name: '고양이',
+    sound: '야옹~',
+    say: function() {
+      console.log(this.sound);
+    }
   };
   
-  const captainAmerica = {
-    name: '스티븐 로저스',
-    actor: '크리스 에반스',
-    alias: '캡틴 아메리카'
-  };
+cat .say = dog.say;
+dog.say();
+cat.say();
 
-  차이
+이전과 똑같이 작동 할 것
 
-  function print(hero) {
-    const text = `${hero.alias}(${hero.name}) 역할을 맡은 배우는 ${
-      hero.actor
-    } 입니다.`;
-    console.log(text);
-  }
-  
-  function print(hero) {
-    const { alias, name, actor } = hero;
-    const text = `${alias}(${name}) 역할을 맡은 배우는 ${actor} 입니다.`;
-    console.log(text);
-  }
-  
-  print(ironMan);
-  print(captainAmerica);
+객체 안에 함수를 넣을 때, 화살표 함수로 선언한다면 제대로 작동하지 않습니다.
+
+이유는, function 으로 선언한 함수는 this 가 제대로 자신이 속한 객체를 가르키게 되는데, 화살표 함수는 그렇지 않기 때문입니다.
