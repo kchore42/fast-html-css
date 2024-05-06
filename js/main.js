0c18a40b9de28e38a39d19bd4d9cc253c54ee506
김현진 <kchore615@gmail.com>
2024-05-07 03:40:12 +0900

    자바스크립트 - 변수

--- js/main.js
+++ js/main.js
@@ -3,3 +3,8 @@ console.log(myName.innerText);
 const myName2 = 1;
 let myName3 = 3;
 myName3 = 4;
+console.log('Hello JavaScript');
+console.log('Hello JavaScript');
+let value = 1;
+console. log(value);
+또 다시 선언 못함
