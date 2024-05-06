c5c3531e182593dd2fe1f62a72b408dc2041c4db
김현진 <kchore615@gmail.com>
2024-05-07 05:15:23 +0900

    함수

--- js/main.js
+++ js/main.js
@@ -1,22 +1,44 @@
-switch/case 문
+05. 함수
+함수는, 특정 코드를 하나의 명령으로 실행 할 수 있게 해주는 기능
 
-switch/case 문은 특정 값이 무엇이냐에 따라 다른 작업을 하고 싶을 때 사용합니다.
+const a = 1;
+const b = 2;
+const sum = a + b;
 
-const device = 'iphone';
+여러 개의 파라미터 가져올 때 쉼표
+결과 값은 return
 
-switch (device) {
-  case 'iphone':
-    console.log('아이폰!');
-    break;
-  case 'ipad':
-    console.log('아이패드!');
-    break;
-  case 'galaxy note':
-    console.log('갤럭시 노트!');
-    break;
-  default:
-    console.log('모르겠네요..');
+function add(a, b) {
+    return a + b;
+  }
+  
+  const sum = add(1, 2);
+  console.log(sum);
+  결과는 3이 됩니다.
+
+  함수를 만들 때는 function 키워드를 사용하며, 함수에서 어떤 값을 받아올지 정해주는데 이를 파라미터(매개변수)라고 부릅니다.
+
+함수 내부에서 return 키워드를 사용하여 함수의 결과물을 지정 할 수 있습니다.
+
+추가적으로, return 을 하게 되면 함수가 끝납니다. 
+
+만약 다음과 같이 코드가 작성된다면, return 아래의 코드는 호출이 안됩니다.
+
+function add(a, b) {
+  return a + b;
+  console.log('호출이 되지 않는 코드!');
 }
-break는 비교 끝났다는 표시
 
-switch/case 문에서는 각 case 에서 실행할 코드를 작성하고 맨 마지막에 break; 를 해주어야 합니다. break 를 하지 않으면 그 다음 case 의 코드까지 실행해버립니다.
+const sum = add(1, 2);
+console.log(sum);
+
+Hello, name!
+name 이라는 파라미터를 넣으면 콘솔에 'Hello name!' 이라는 결과를 출력하는 코드를 작성해봅시다.
+
+function hello(name) {
+  console.log('Hello, ' + name + '!');
+}
+hello('velopert');
+결과물은 다음과 같습니다.
+
+"Hello, velopert!"
