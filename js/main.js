ac247e5af43d854daffac8a531641dc5a94630d9
김현진 <kchore615@gmail.com>
2024-05-07 18:58:31 +0900

    배열 내장함수 - map

--- js/main.js
+++ js/main.js
@@ -1,25 +1,51 @@
-연습
-numbers 라는 배열을 파라미터로 받아서 총합을 구하는 함수
+map
+map 은 배열 안의 각 원소를 변환 할 때 사용 되며, 이 과정에서 새로운 배열이 만들어진다.
 
-function sumOf(numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-  }
-  return sum;
+만약에 배열 안의 모든 숫자를 제곱해서 새로운 배열을 만들고 싶다면 어떻게 해야 할까요?
+
+for 구문
+const array = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const squared = [];
+for (let i = 0; i < array.length; i++) {
+  squared.push(array[i] * array[i]);
 }
 
-const result = sumOf([1, 2, 3, 4, 5]);
-console.log(result);
-결과는 15 
+console.log(squared);
 
-function sumOf(numbers) {
-  let sum = o;
-  for(let i = o; i < numbers.length; i++) {
-    sum += numbersi[i];
-  }
-  return sum;
-}
 
-const result = sumOf([1, 2, 3, 4, 5]);
-console.log(result)
+forEach 구문
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const squared = [];
+
+array.forEach(n => {
+  squared.push(n * n);
+});
+
+console.log(squared);
+
+결과는 다음과 같다.
+
+[1, 4, 9, 16, 25, 36, 49, 64];
+
+만약 map 을 사용하면 이를 더 짧은 코드를 사용하여 구현 할 수 있습니다.
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8];
+
+const square = n => n * n;
+const squared = array.map(square);
+console.log(squared);
+
+
+map 함수의 파라미터로는 변화를 주는 함수를 전달해줍니다. 이를 변화함수라고 부르도록 하겠습니다.
+
+현재 우리의 변화함수 square 는 파라미터 n 을 받아와서 이를 제곱해줍니다.
+
+array.map 함수를 사용 할 때 square 를 변화함수로 사용함으로서, 내부의 모든 값에 대하여 제곱을 해서 새로운 배열을 생성
+
+변화 함수를 꼭 이름을 붙여서 선언 할 필요는 없습니다. 코드를 다음과 같이 작성해도 됩니다.
+
+const squared = array.map(n => n * n);
+console.log(squared);
