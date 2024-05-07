458191b0e4130cf494cbcdbbb593a29b6b56a152
김현진 <kchore615@gmail.com>
2024-05-07 11:35:31 +0900

    반복문 - for

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
- 
-const object = [
-   { name: '멍멍이'},
-   { name: '야옹이'}
-  ];
-  console.log(objects.length)
+for
+for 문은 가장 기본적인 반복문
 
-objects. push({
-  name: '멍뭉이'
-});
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
 
-console.log(objects.length)
+for (초기 구문; 조건 구문; 변화 구문;) {
+  코드
+}
 
+for 문을 사용 할 때 보통 i++ 를 해서 1씩 증감하는 형태로 사용합니다. 그런데, 1씩 빼는 형태도 가능
 
-배열에 새 항목 추가하기
-배열에 새로운 항목을 추가 할 때에는 배열이 지니고있는 내장 함수인 push 함수를 사용합니다.
+for (let i = 10; i > 0; i--) {
+  console.log(i);
+}
 
+배열과 for
+이번에는 우리가 이전에 배운 배열과 for 문을 함꼐 활용
 
-  
-const array [1, 2, 3]
-array push(6)
+const names = ['멍멍이', '야옹이', '멍뭉이'];
 
-console.log(array.length)
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
