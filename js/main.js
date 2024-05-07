5a181e81575a1596ab8496f54737b3f88fe139ba
김현진 <kchore615@gmail.com>
2024-05-07 11:44:31 +0900

    break 와 continue

--- js/main.js
+++ js/main.js
@@ -1,41 +1,11 @@
-반복문 - for...of, for...in
+break 와 continue
+반복문 안에서는 break 와 continue 를 통하여 반복문에서 벗어나거나, 그 다음 루프를 돌게끔 할 수 있음
 
-for...of
-for...of문은 배열에 관한 반복문을 돌리기 위해서 만들어진 반복문
-
-let numbers = [10, 20, 30, 40, 50];
-for (let number of numbers) {
-  console.log(number);
+for (let i = 0; i < 10; i++) {
+  if (i === 2) continue; // 다음 루프를 실행
+  console.log(i);
+  if (i === 5) break; // 반복문을 끝내기
 }
+i 가 2 일땐 continue 를 하여 원래 console.log 를 해야 하지만 그 코드를 수행하지 않고 바로 3으로 넘어감.
 
-
-객체를 위한 반복문 for...in
-
-const doggy = {
-  name: '멍멍이',
-  sound: '멍멍',
-  age: 2
-};
-
-console.log(Object.entries(doggy));
-console.log(Object.keys(doggy));
-console.log(Object.values(doggy));
-
-각 함수의 역할은 다음과 같습니다.
-
-Object.entries: [[키, 값], [키, 값]] 형태의 배열로 변환
-Object.keys: [키, 키, 키] 형태의 배열로 변환
-Object.values: [값, 값, 값] 형태의 배열로 변환
-
-객체가 지니고 있는 값에 대하여 반복을 하고 싶다면 위 함수들을 사용해도 되고, 
-for...in 구문을 사용해도 됨.
-
-const doggy = {
-  name: '멍멍이',
-  sound: '멍멍',
-  age: 2
-};
-
-for (let key in doggy) {
-  console.log(`${key}: ${doggy[key]}`);
-}
+i 가 5 일땐 break 를하여 반복문을 종료
