02880d1c6e1bfa65ee3b9da940e441f885112cf6
김현진 <kchore615@gmail.com>
2024-05-07 12:05:43 +0900

    연습

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
-break 와 continue
-반복문 안에서는 break 와 continue 를 통하여 반복문에서 벗어나거나, 그 다음 루프를 돌게끔 할 수 있음
+연습
+numbers 라는 배열을 파라미터로 받아서 총합을 구하는 함수
 
-for (let i = 0; i < 10; i++) {
-  if (i === 2) continue; // 다음 루프를 실행
-  console.log(i);
-  if (i === 5) break; // 반복문을 끝내기
+function sumOf(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
 }
-i 가 2 일땐 continue 를 하여 원래 console.log 를 해야 하지만 그 코드를 수행하지 않고 바로 3으로 넘어감.
 
-i 가 5 일땐 break 를하여 반복문을 종료
+const result = sumOf([1, 2, 3, 4, 5]);
+console.log(result);
+결과는 15 
+
+function sumOf(numbers) {
+  let sum = o;
+  for(let i = o; i < numbers.length; i++) {
+    sum += numbersi[i];
+  }
+  return sum;
+}
+
+const result = sumOf([1, 2, 3, 4, 5]);
+console.log(result)
