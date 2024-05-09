0a2e8bcc8df438a16f1f8495eecb6a65f5ad8fef
김현진 <kchore615@gmail.com>
2024-05-09 14:06:32 +0900

    프로토 타입 - 객체 생성자 상속하기

--- js/main.js
+++ js/main.js
@@ -1,19 +1,4 @@
-function Animal(type, name, sound) {
-  this.type = type;
-  this.name = name;
-  this.sound = sound;
-}
-
-const dog = new Animal('개', '멍멍이', '멍멍');
-const cat = new Animal('고양이', '야옹이', '야옹');
-
-dog.say();
-cat.say();
-
-객체 생성시 대문자, new 사용
-
-프로토타입
-프로토타입은 다음과 같이 객체 생성자 함수 아래에 .prototype.[원하는키] = 코드를 입력하여 설정 할 수 있습니다.
+객체 생성자 상속받기
 
 function Animal(type, name, sound) {
   this.type = type;
@@ -26,11 +11,18 @@ Animal.prototype.say = function() {
 };
 Animal.prototype.sharedValue = 1;
 
-const dog = new Animal('개', '멍멍이', '멍멍');
-const cat = new Animal('고양이', '야옹이', '야옹');
+function Dog(name, sound) {
+  Animal.call(this, '개', name, sound);
+}
+Dog.prototype = Animal.prototype;
+
+function Cat(name, sound) {
+  Animal.call(this, '고양이', name, sound);
+}
+Cat.prototype = Animal.prototype;
+
+const dog = new Dog('멍멍이', '멍멍');
+const cat = new Cat('야옹이', '야옹');
 
 dog.say();
 cat.say();
-
-console.log(dog.sharedValue);
-console.log(cat.sharedValue);
