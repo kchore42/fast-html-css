e6d17dd7e4b3aadfe85168bcec65e6ac5ecf129c
김현진 <kchore615@gmail.com>
2024-05-09 14:38:34 +0900

    classs

--- js/main.js
+++ js/main.js
@@ -1,25 +1,44 @@
-객체 생성자 상속받기
-
-function Animal(type, name, sound) {
-  this.type = type;
-  this.name = name;
-  this.sound = sound;
+class Animal {
+  constructor(type, name, sound) {
+    this.type = type;
+    this.name = name;
+    this.sound = sound;
+  }
+  say() {
+    console.log(this.sound);
+  }
 }
 
-Animal.prototype.say = function() {
-  console.log(this.sound);
-};
-Animal.prototype.sharedValue = 1;
+const dog = new Animal('개', '멍멍이', '멍멍');
+const cat = new Animal('고양이', '야옹이', '야옹');
 
-function Dog(name, sound) {
-  Animal.call(this, '개', name, sound);
-}
-Dog.prototype = Animal.prototype;
+dog.say();
+cat.say();
 
-function Cat(name, sound) {
-  Animal.call(this, '고양이', name, sound);
+class 를 사용했을때에는, 다른 클래스를 쉽게 상속 할 수 있습니다.
+
+class Animal {
+  constructor(type, name, sound) {
+    this.type = type;
+    this.name = name;
+    this.sound = sound;
+  }
+  say() {
+    console.log(this.sound);
+  }
 }
-Cat.prototype = Animal.prototype;
+
+class Dog extends Animal {
+  constructor(name, sound) {
+    super('개', name, sound);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, sound) {
+    super('고양이', name, sound);
+  }
+}
 
 const dog = new Dog('멍멍이', '멍멍');
 const cat = new Cat('야옹이', '야옹');
