c50e7d2b15447ac48f90b916003659c966816b00
김현진 <kchore615@gmail.com>
2024-05-10 07:02:12 +0900

    || 연산자로 코드 단축시키기

--- js/main.js
+++ js/main.js
@@ -1,65 +1,33 @@
-단축평가 논리 계산 법
+|| 연산자로 코드 단축시키기
 
-true && true // true
-true && false // false
-true || false // true
-false || true // true
-
-const dog = {
-  name: '멍멍이'
+const namelessDog = {
+  name: ''
 };
 
 function getName(animal) {
-  return animal.name;
-}
-
-const name = getName(dog);
-console.log(name); // 멍멍이
-
-const dog = {
-  name: '멍멍이'
-};
-
-function getName(animal) {
-  if (animal) {
-    return animal.name;
+  const name = animal && animal.name;
+  if (!name) {
+    return '이름이 없는 동물입니다';
   }
-  return undefined;
+  return name;
 }
 
-const name = getName();
-console.log(name);
+const name = getName(namelessDog);
+console.log(name); // 이름이 없는 동물입니다.
 
-const dog = {
-  name: '멍멍이'
+위 코드는 || 연산자를 사용하면 다음과 같이 단축시킬 수 있습니다.
+
+A || B 는 만약 A 가 Truthy 할경우 결과는 A 가 됩니다. 
+반면, A 가 Falsy 하다면 결과는 B 가 됩니다.
+
+const namelessDog = {
+  name: ''
 };
 
 function getName(animal) {
-  return animal && animal.name;
+  const name = animal && animal.name;
+  return name || '이름이 없는 동물입니다.';
 }
 
-const name = getName();
-console.log(name); // undefined
-
-const dog = {
-  name: '멍멍이'
-};
-
-function getName(animal) {
-  return animal && animal.name;
-}
-
-const name = getName(dog);
-console.log(name); // 멍멍이
-이게 작동하는 이유는, A && B 연산자를 사용하게 될 때에는 A 가 Truthy 한 값이라면, B 가 결과값이 됩니다. 
-반면, A 가 Falsy 한 값이라면 결과는 A 가 됩니다.
-
-console.log(true && 'hello'); // hello
-console.log(false && 'hello'); // false
-console.log('hello' && 'bye'); // bye
-console.log(null && 'hello'); // null
-console.log(undefined && 'hello'); // undefined
-console.log('' && 'hello'); // ''
-console.log(0 && 'hello'); // 0
-console.log(1 && 'hello'); // hello
-console.log(1 && 1); // 1
+const name = getName(namelessDog);
+console.log(name); // 이름이 없는 동물입니다.
