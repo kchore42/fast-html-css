0d911bac95dd5511d80f9c9b8415235d82a63587
kchore42 <155278187+kchore42@users.noreply.github.com>
2024-05-10 16:34:11 +0900

    05. 조건문 더 스마트하게 쓰기 -1

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
-forEach
+05. 조건문 더 스마트하게 쓰기
 
-forEach 는 가장 쉬운 배열 내장함수
+특정 값이 여러 값 중 하나인지 확인해야 할 때
+만약, 특정 값이 여러 값 중 하나인지 확인을 해야 하는 상황이이라면!
 
-기존에 우리가 배웠던 for 문을 대체 시킬 수 있다.
+그러면, 이러한 시도를 할 수도 있을 것입니다.
 
-for 문
-
-const superheroes = ['아이언맨', '캡틴 아메리카', '토르', '닥터 스트레인지'];
-
-for (let i = 0; i < superheroes.length; i++) {
-  console.log(superheroes[i]);
+function isAnimal(text) {
+  return (
+    text === '고양이' || text === '개' || text === '거북이' || text === '너구리'
+  );
 }
 
-배열의 forEach 함수를 사용하면 다음과 같이 구현 할 수 있습니다.
+console.log(isAnimal('개')); // true
+console.log(isAnimal('노트북')); // false
+비교해야 할 값이 많아질 수록 코드는 길어집니다.
 
-const superheroes = ['아이언맨', '캡틴 아메리카', '토르', '닥터 스트레인지'];
+이러한 코드를 간단하게 해결 할 수 있는 방법은, 
+배열을 만들고 배열의 includes 함수를 사용하는 것 
 
-superheroes.forEach(hero => {
-  console.log(hero);
-});
-forEach 함수의 파라미터로는, 각 원소에 대하여 처리하고 싶은 코드를 함수로 넣어줌. 이 함수의 파라미터 hero는 각 원소를 가르키게 됨.
+function isAnimal(name) {
+  const animals = ['고양이', '개', '거북이', '너구리'];
+  return animals.includes(name);
+}
 
-이렇게 함수형태의 파라미터를 전달하는 것을 콜백함수 라고 부름. 함수를 등록해주면, forEach 가 실행을 해주는 기능
+console.log(isAnimal('개')); // true
+console.log(isAnimal('노트북')); // false
+
+원한다면, animals 배열을 선언하는 것도 생략하고, 화살표 함수로 작성할 수도 있습니다.
+
+const isAnimal = name => ['고양이', '개', '거북이', '너구리'].includes(name);
+
+console.log(isAnimal('개')); // true
+console.log(isAnimal('노트북')); // false
+
+물론, 코드가 짧다고 해서 무조건 좋은것은 아닙니다. 
+단, 코드가 짧으면서도 읽었을 때 어떤 역할을 하는지 잘 이해가 될 수 있어야 비로소 좋은 코드
