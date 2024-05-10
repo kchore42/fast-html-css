e394be17299c92cc6b30fa7e7b4621884759eed8
김현진 <kchore615@gmail.com>
2024-05-10 19:12:15 +0900

    함수 인자와 spread

--- js/main.js
+++ js/main.js
@@ -1,49 +1,37 @@
-함수 파라미터에서의 rest
-rest 를 함수파라미터에서 사용 할 수도 있다. 
+함수 인자와 spread
 
-예를 들어서 우리가 파라미터로 넣어준 모든 값들을 합해주는 함수를 만들어주고 싶다고 가정
-
-function sum(a, b, c, d, e, f, g) {
-  let sum = 0;
-  if (a) sum += a;
-  if (b) sum += b;
-  if (c) sum += c;
-  if (d) sum += d;
-  if (e) sum += e;
-  if (f) sum += f;
-  if (g) sum += g;
-  return sum;
+const myFunction(a) { // 여기서 a 는 파라미터
+  console.log(a); // 여기서 a 는 인자
 }
 
-const result = sum(1, 2, 3, 4, 5, 6);
-console.log(result);
+myFunction('hello world'); // 여기서 'hello world' 는 인자
+함수에서 값을 읽을때, 그 값들은 파라미터라고 부릅니다. 그리고 함수에서 값을 넣어줄 때, 그 값들은 인자라고 부릅니다.
 
-위에서의 sum 함수는 7개의 파라미터를 받아오는데, 아래에서 사용 할때에는 6개만 넣어줬습니다. 
+우리가 방금 함수파라미터와 rest 를 사용한 것과 비슷한데, 반대의 역할입니다. 
 
-그러면, g 값이 undefined 가 되기 때문에 sum 에 더하는 과정에서 += undefined 를 하게 되면 결과는 
-NaN 이 되버립니다. 
-
-그렇기 때문에 함수에서 하나하나 유효한 값인지 확인을 해줬지요.
-
-함수의 파라미터가 몇개가 될 지 모르는 상황에서 rest 파라미터를 사용하면 매우 유용하다. 
-코드를 다음과 같이 수정해보자.
-
-function sum(...rest) {
-  return rest;
-}
-
-const result = sum(1, 2, 3, 4, 5, 6);
-console.log(result);
-
-
-result 가 가르키고 있는 것은 함수에서 받아온 파라미터들로 이루어진 배열이다.
-
-우리가 이제 파라미터들이 들어가있는 배열을 받았으니, 그냥 모두 더해주면 되겠지?
+예를 들어서, 우리가 배열 안에 있는 원소들을 모두 파라미터로 넣어주고 싶다고 가정해보겠습니다.
 
 function sum(...rest) {
   return rest.reduce((acc, current) => acc + current, 0);
 }
 
-const result = sum(1, 2, 3, 4, 5, 6);
-console.log(result); // 21
-여기서 reduce 함수가 사용 됐는데, reduce 를 잘 모르겠으면 1장 섹션 9를 복습
+const numbers = [1, 2, 3, 4, 5, 6];
+const result = sum(
+  numbers[0],
+  numbers[1],
+  numbers[2],
+  numbers[3],
+  numbers[4],
+  numbers[5]
+);
+console.log(result);
+
+sum함수를 사용 할 때 인자 부분에서 spread 를 사용하면 다음과 같이 표현이 가능
+
+function sum(...rest) {
+  return rest.reduce((acc, current) => acc + current, 0);
+}
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const result = sum(...numbers);
+console.log(result);
