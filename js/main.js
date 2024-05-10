c8dafdd10e6264cb3f93afd5983e601f2011b28b
김현진 <kchore615@gmail.com>
2024-05-10 19:04:20 +0900

    배열에서의 rest

--- js/main.js
+++ js/main.js
@@ -1,51 +1,18 @@
-rest
+배열에서의 rest
+다음, 배열에서의 사용 예시를 알아봅시다.
 
-rest는 객체, 배열, 그리고 함수의 파라미터에서 사용이 가능
+const numbers = [0, 1, 2, 3, 4, 5, 6];
 
-객체에서의 rest
+const [one, ...rest] = numbers;
 
-우선 객체에서의 예시를 알아볼까요?
-
-const purpleCuteSlime = {
-  name: '슬라임',
-  attribute: 'cute',
-  color: 'purple'
-};
-
-const { color, ...rest } = purpleCuteSlime;
-console.log(color);
+console.log(one);
 console.log(rest);
 
 
-rest 안에 color 값을 제외한 값이 들어있다
+배열 비구조화 할당을 통하여 원하는 값을 밖으로 꺼내고, 나머지 값을 rest 안에 넣었습니다.
 
-rest 는 객체와 배열에서 사용 할 때는 이렇게 비구조화 할당 문법과 함께 사용된다. 
-주로 사용 할때는 위와 같이 rest 라는 키워드를 사용하게 되는데요, 
-추출한 값의 이름이 꼭 rest 일 필요는 없다.
+반면 이렇게 할 수는 없답니다.
 
-const purpleCuteSlime = {
-  name: '슬라임',
-  attribute: 'cute',
-  color: 'purple'
-};
+const numbers = [0, 1, 2, 3, 4, 5, 6];
 
-const { color, ...cuteSlime } = purpleCuteSlime;
-console.log(color);
-console.log(cuteSlime);
-이렇게 해도 무방합니다.
-
-이어서, attribute 까지 없앤 새로운 객체를 만들고 싶다면 이렇게 해주면 ok
-
-const purpleCuteSlime = {
-  name: '슬라임',
-  attribute: 'cute',
-  color: 'purple'
-};
-
-const { color, ...cuteSlime } = purpleCuteSlime;
-console.log(color);
-console.log(cuteSlime);
-
-const { attribute, ...slime } = cuteSlime;
-console.log(attribute);
-console.log(slime);
+const [..rest, last] = numbers;
