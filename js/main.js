657bdbbb1ce7397e7ec602b6bb658d5da423cd8c
김현진 <kchore615@gmail.com>
2024-05-10 19:00:32 +0900

    객체에서의 rest

--- js/main.js
+++ js/main.js
@@ -1,33 +1,51 @@
-|| 연산자로 코드 단축시키기
+rest
 
-const namelessDog = {
-  name: ''
+rest는 객체, 배열, 그리고 함수의 파라미터에서 사용이 가능
+
+객체에서의 rest
+
+우선 객체에서의 예시를 알아볼까요?
+
+const purpleCuteSlime = {
+  name: '슬라임',
+  attribute: 'cute',
+  color: 'purple'
 };
 
-function getName(animal) {
-  const name = animal && animal.name;
-  if (!name) {
-    return '이름이 없는 동물입니다';
-  }
-  return name;
-}
+const { color, ...rest } = purpleCuteSlime;
+console.log(color);
+console.log(rest);
 
-const name = getName(namelessDog);
-console.log(name); // 이름이 없는 동물입니다.
 
-위 코드는 || 연산자를 사용하면 다음과 같이 단축시킬 수 있습니다.
+rest 안에 color 값을 제외한 값이 들어있다
 
-A || B 는 만약 A 가 Truthy 할경우 결과는 A 가 됩니다. 
-반면, A 가 Falsy 하다면 결과는 B 가 됩니다.
+rest 는 객체와 배열에서 사용 할 때는 이렇게 비구조화 할당 문법과 함께 사용된다. 
+주로 사용 할때는 위와 같이 rest 라는 키워드를 사용하게 되는데요, 
+추출한 값의 이름이 꼭 rest 일 필요는 없다.
 
-const namelessDog = {
-  name: ''
+const purpleCuteSlime = {
+  name: '슬라임',
+  attribute: 'cute',
+  color: 'purple'
 };
 
-function getName(animal) {
-  const name = animal && animal.name;
-  return name || '이름이 없는 동물입니다.';
-}
+const { color, ...cuteSlime } = purpleCuteSlime;
+console.log(color);
+console.log(cuteSlime);
+이렇게 해도 무방합니다.
 
-const name = getName(namelessDog);
-console.log(name); // 이름이 없는 동물입니다.
+이어서, attribute 까지 없앤 새로운 객체를 만들고 싶다면 이렇게 해주면 ok
+
+const purpleCuteSlime = {
+  name: '슬라임',
+  attribute: 'cute',
+  color: 'purple'
+};
+
+const { color, ...cuteSlime } = purpleCuteSlime;
+console.log(color);
+console.log(cuteSlime);
+
+const { attribute, ...slime } = cuteSlime;
+console.log(attribute);
+console.log(slime);
