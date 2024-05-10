698baafa68e553f5a4e14d3488802c3133832869
김현진 <kchore615@gmail.com>
2024-05-10 19:08:20 +0900

    함수 파라미터에서의 rest

--- js/main.js
+++ js/main.js
@@ -1,18 +1,49 @@
-배열에서의 rest
-다음, 배열에서의 사용 예시를 알아봅시다.
+함수 파라미터에서의 rest
+rest 를 함수파라미터에서 사용 할 수도 있다. 
 
-const numbers = [0, 1, 2, 3, 4, 5, 6];
+예를 들어서 우리가 파라미터로 넣어준 모든 값들을 합해주는 함수를 만들어주고 싶다고 가정
 
-const [one, ...rest] = numbers;
+function sum(a, b, c, d, e, f, g) {
+  let sum = 0;
+  if (a) sum += a;
+  if (b) sum += b;
+  if (c) sum += c;
+  if (d) sum += d;
+  if (e) sum += e;
+  if (f) sum += f;
+  if (g) sum += g;
+  return sum;
+}
 
-console.log(one);
-console.log(rest);
+const result = sum(1, 2, 3, 4, 5, 6);
+console.log(result);
+
+위에서의 sum 함수는 7개의 파라미터를 받아오는데, 아래에서 사용 할때에는 6개만 넣어줬습니다. 
+
+그러면, g 값이 undefined 가 되기 때문에 sum 에 더하는 과정에서 += undefined 를 하게 되면 결과는 
+NaN 이 되버립니다. 
+
+그렇기 때문에 함수에서 하나하나 유효한 값인지 확인을 해줬지요.
+
+함수의 파라미터가 몇개가 될 지 모르는 상황에서 rest 파라미터를 사용하면 매우 유용하다. 
+코드를 다음과 같이 수정해보자.
+
+function sum(...rest) {
+  return rest;
+}
+
+const result = sum(1, 2, 3, 4, 5, 6);
+console.log(result);
 
 
-배열 비구조화 할당을 통하여 원하는 값을 밖으로 꺼내고, 나머지 값을 rest 안에 넣었습니다.
+result 가 가르키고 있는 것은 함수에서 받아온 파라미터들로 이루어진 배열이다.
 
-반면 이렇게 할 수는 없답니다.
+우리가 이제 파라미터들이 들어가있는 배열을 받았으니, 그냥 모두 더해주면 되겠지?
 
-const numbers = [0, 1, 2, 3, 4, 5, 6];
+function sum(...rest) {
+  return rest.reduce((acc, current) => acc + current, 0);
+}
 
-const [..rest, last] = numbers;
+const result = sum(1, 2, 3, 4, 5, 6);
+console.log(result); // 21
+여기서 reduce 함수가 사용 됐는데, reduce 를 잘 모르겠으면 1장 섹션 9를 복습
