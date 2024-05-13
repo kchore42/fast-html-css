007f7fa72a4fae2c5104a6afc0289b969e2ff529
김현진 <kchore615@gmail.com>
2024-05-14 07:16:09 +0900

    scope 이해 - hosting

--- js/main.js
+++ js/main.js
@@ -1,123 +1,65 @@
-자바스크립트의 Scope 에 대한 이해
+Hoisting 이해하기
 
-Scope 란, 우리가 변수 혹은 함수를 선언하게 될 때 해당 변수 또는 함수가 유효한 범위를 의미
+Hoisting 이란, 
+자바스크립트에서 아직 선언되지 않은 함수/변수를 "끌어올려서" 사용 할 수 있는 자바스크립트의 작동 방식을 의미
 
-Scope 는 총 3가지 종류가 있다.
-
-Global (전역) Scope: 코드의 모든 범위에서 사용이 가능
-Function (함수) Scope: 함수 안에서만 사용이 가눙
-Block (블록) Scope: if, for, switch 등 특정 블록 내부에서만 사용이 가능
-
-예시를 통한 Scope 이해
-
-const value = 'hello!';
+myFunction();
 
 function myFunction() {
-  console.log('myFunction: ');
-  console.log(value);
+  console.log('hello world!');
 }
+위 코드에서는 myFunction 함수를 선언하기 전에, myFunction() 을 호출해주었습니다. 
 
-function otherFunction() {
-  console.log('otherFunction: ');
-  const value = 'bye!';
-  console.log(value);
+함수가 아직 선언되지 않았음에도 불구하고 코드는 정상적으로 작동하게 됩니다.
+
+이게 잘 작동하는 이유는, 자바스크립트 엔진이 위 코드를 해석하는 과정에서, 
+
+다음과 같이 받아들이게 되기 때문입니다.
+
+function myFunction() {
+  console.log('hello world!');
 }
 
 myFunction();
-otherFunction();
+이러한 현상을, Hoisting 이라고 부릅니다.
 
-console.log('global scope: ');
-console.log(value);
+변수 또한 Hoisting 됩니다.
 
-위 코드의 결과
-//myFunction: hello!
-//otherFunction: bye!
-//global scope: hello!
+예시
 
-또 다른 예시
+console.log(number);
+//오류 발생
 
-const value = 'hello!';
+console.log(number);
+var number = 2;
 
-function myFunction() {
-  const value = 'bye!';
-  const anotherValue = 'world';
-  function functionInside() {
-    console.log('functionInside: ');
-    console.log(value);
-    console.log(anotherValue);
-  }
-  functionInside();
+//undefined
+
+자바스크립트 엔진이 위 코드를 해석 할 때는 다음과 같이 받아들이게 됩니다.
+
+var number;
+console.log(number);
+number = 2;
+
+반면, const 와 let 은 hoisting 이 발생하지 않고, 에러가 발생하게 됩니다. 
+Codesandbox 에서는 자바스크립트가 Babel 이라는 도구에 의하여 
+const 와 let 이 var 로 변환되기 때문에 오류가 발생하지 않습니다. 
+
+function fn() {
+  console.log(a);
+  let a = 2;
 }
+fn();
 
+//오류 발생
 
-myFunction()
-console.log('global scope: ');
-console.log(value);
-console.log(anotherValue);
+Hoisting 은 자바스크립트 엔진이 갖고 있는 성질이며, 
+Hoisting 을 일부러 할 필요는 없지만, 방지하는 것이 좋습니다. 
+왜냐하면 Hoisting 이 발생하는 코드는 이해하기 어렵기 때문에 유지보수도 힘들어지고 
+의도치 않는 결과물이 나타나기 쉽기 때문입니다.
 
-//function: bye!, world
-//global scope: hello!
-//Error in sandbox: another is not defined
+Hoisting 을 방지하기 위해서, 함수의 경우 꼭 선언 후에 호출을 하도록 주의를 하시고, 
+var 대신 const, let 을 위주로 사용하세요. 
+추가적으로, 나중에 자바스크립트 개발을 본격적으로 하게 될 때에는 
+ESLint 라는것을 사용하여 Hoisting 이 발생하는 코드는 에디터상에서 쉽게 발견
 
-myFunction 내부에 anotherValue 라는 새로운 값을 선언했고, functionInside 라는 함수도 선언을 했습니다. functionInside 함수에서는 myFunction 에서 선언한 value 값과 anotherValue 값을 조회 할 수 있습니다.
-
-반면, myFunction 밖에서는 anotherValue 를 조회 할 수 없습니다.
-
-이제, 또 다른 예시
-
-const value = 'hello!';
-
-function myFunction() {
-  const value = 'bye!';
-  if (true) {
-    const value = 'world';
-    console.log('block scope: ');
-    console.log(value);
-  }
-  console.log('function scope: ');
-  console.log(value);
-}
-
-myFunction();
-console.log('global scope: ');
-console.log(value);
-
-위 코드의 결과
-//block scope: world
-//function scope: bye!
-//global scope: hello!
-
-const 로 선언한 값은 Block Scope 로 선언이 됩니다. 
-따라서, if 문 같은 블록 내에서 새로운 변수/상수를 선언하게 된다면, 
-해당 블록 내부에서만 사용이 가능하며, 
-블록 밖의 범위에서 똑같은 이름을 가진 값이 있다고 해도 영향을 주지 않습니다.
-
-let 또한 마찬가지
-
-
-하지만 var 는 어떨까요?
-
-var value = 'hello!';
-
-function myFunction() {
-  var value = 'bye!';
-  if (true) {
-    var value = 'world';
-    console.log('block scope: ');
-    console.log(value);
-  }
-  console.log('function scope: ');
-  console.log(value);
-}
-
-myFunction();
-console.log('global scope: ');
-console.log(value);
-
-var 는 Function Scope 로 선언이 되므로, 
-if 문 블록 내부에서 선언한 value 값이 블록 밖의 value 에도 영향을 미치게 됩니다.
-
-위 코드의 결과
-//block scope: world
-//function scope: world
-//global scope: hello!
