199c071dcb9ee64e9b975d040608048715a231a6
kchore42 <155278187+kchore42@users.noreply.github.com>
2024-05-14 18:05:53 +0900

    02. async/await

--- main.js
+++ main.js
@@ -1,204 +1,196 @@
-01. Promise
-프로미스는 비동기 작업을 조금 더 편하게 처리 할 수 있도록 ES6 에 도입된 기능입니다. 
-이전에는 비동기 작업을 처리 할 때에는 콜백 함수로 처리를 해야 했었는데요, 
-콜백 함수로 처리를 하게 된다면 비동기 작업이 많아질 경우 코드가 쉽게 난잡해지게 되었습니다.
+02. async/await
+async/await 문법은 ES8에 해당하는 문법으로서, Promise 를 더욱 쉽게 사용 할 수 있게 해줍니다.
 
-한번 숫자 n 을 파라미터로 받아와서 다섯번에 걸쳐 1초마다 1씩 더해서 출력하는 작업을 
-setTimeout 으로 구현해봅시다.
+기본적인 사용법을 알아봅시다.
 
-function increaseAndPrint(n, callback) {
-  setTimeout(() => {
-    const increased = n + 1;
-    console.log(increased);
-    if (callback) {
-      callback(increased);
-    }
-  }, 1000);
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-increaseAndPrint(0, n => {
-  increaseAndPrint(n, n => {
-    increaseAndPrint(n, n => {
-      increaseAndPrint(n, n => {
-        increaseAndPrint(n, n => {
-          console.log('끝!');
-        });
-      });
-    });
-  });
-});
-코드 읽기가 복잡하죠? 이런 식의 코드를 Callback Hell (콜백지옥) 이라고 부릅니다.
-
-비동기적으로 처리해야 하는 일이 많아질수록, 코드의 깊이가 계속 깊어지는 현상이 있는데요, 
-Promise 를 사용하면 이렇게 코드의 깊이가 깊어지는 현상을 방지 할 수 있습니다.
-
-Promise 만들기
-
-Promise 는 다음과 같이 만듭니다.
-
-const myPromise = new Promise((resolve, reject) => {
-  // 구현..
-})
-Promise 는 성공 할 수도 있고, 실패 할 수도 있습니다. 
-성공 할 때에는 resolve 를 호출해주면 되고, 실패할 때에는 reject 를 호출해주면 됩니다. 
-지금 당장은 실패하는 상황은 고려하지 않고, 1초 뒤에 성공시키는 상황에 대해서만 구현을 해보겠습니다.
-
-const myPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(1);
-  }, 1000);
-});
-
-myPromise.then(n => {
-  console.log(n);
-});
-resolve 를 호출 할 때 특정 값을 파라미터로 넣어주면, 이 값을 작업이 끝나고 나서 사용 할 수 있습니다. 
-작업이 끝나고 나서 또 다른 작업을 해야 할 때에는 Promise 뒤에 .then(...) 을 붙여서 사용하면 됩니다.
-
-이번에는, 1초뒤에 실패되게끔 해봅시다.
-
-const myPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    reject(new Error());
-  }, 1000);
-});
-
-myPromise
-  .then(n => {
-    console.log(n);
-  })
-  .catch(error => {
-    console.log(error);
-  });
-실패하는 상황에서는 reject 를 사용하고, .catch 를 통하여 실패했을시 수행 할 작업을 설정 할 수 있습니다.
-
-이제, Promise 를 만드는 함수를 작성해봅시다.
-
-function increaseAndPrint(n) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const value = n + 1;
-      if (value === 5) {
-        const error = new Error();
-        error.name = 'ValueIsFiveError';
-        reject(error);
-        return;
-      }
-      console.log(value);
-      resolve(value);
-    }, 1000);
-  });
+async function process() {
+  console.log('안녕하세요!');
+  await sleep(1000); // 1초쉬고
+  console.log('반갑습니다!');
 }
 
-increaseAndPrint(0).then((n) => {
-  console.log('result: ', n);
-})
+process();
 
+async/await 문법을 사용할 때에는, 함수를 선언 할 때 함수의 앞부분에 async 키워드를 붙여주세요. 
+그리고 Promise 의 앞부분에 await 을 넣어주면 해당 프로미스가 끝날때까지 기다렸다가 다음 작업을 수행 할 수 있습니다.
 
-여기까지만 보면, 결국 함수를 전달하는건데, 뭐가 다르지 싶을수도 있습니다. 
-하지만, Promise 의 속성 중에는, 만약 then 내부에 넣은 함수에서 또 Promise 를 리턴하게 된다면, 
-연달아서 사용 할 수 있습니다. 
+위 코드에서는 sleep 이라는 함수를 만들어서 파라미터로 넣어준 시간 만큼 기다리는 Promise 를 만들고, 
+이를 process 함수에서 사용해주었습니다.
 
-이제, Promise 를 만드는 함수를 작성해봅시다.
+함수에서 async 를 사용하면, 해당 함수는 결과값으로 Promise 를 반환하게 됩니다.
+따라서 다음과 같이 코드를 작성 할 수 있습니다.
 
-function increaseAndPrint(n) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const value = n + 1;
-      if (value === 5) {
-        const error = new Error();
-        error.name = 'ValueIsFiveError';
-        reject(error);
-        return;
-      }
-      console.log(value);
-      resolve(value);
-    }, 1000);
-  });
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-increaseAndPrint(0).then((n) => {
-  console.log('result: ', n);
-})
-
-
-여기까지만 보면, 결국 함수를 전달하는건데, 뭐가 다르지 싶을수도 있습니다. 
-하지만, Promise 의 속성 중에는, 만약 then 내부에 넣은 함수에서 또 Promise 를 리턴하게 된다면, 
-연달아서 사용 할 수 있습니다. 
-
-
-다음과 같이 말이죠.
-
-function increaseAndPrint(n) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const value = n + 1;
-      if (value === 5) {
-        const error = new Error();
-        error.name = 'ValueIsFiveError';
-        reject(error);
-        return;
-      }
-      console.log(value);
-      resolve(value);
-    }, 1000);
-  });
+async function process() {
+  console.log('안녕하세요!');
+  await sleep(1000); // 1초쉬고
+  console.log('반갑습니다!');
 }
 
-increaseAndPrint(0)
-  .then(n => {
-    return increaseAndPrint(n);
-  })
-  .then(n => {
-    return increaseAndPrint(n);
-  })
-  .then(n => {
-    return increaseAndPrint(n);
-  })
-  .then(n => {
-    return increaseAndPrint(n);
-  })
-  .then(n => {
-    return increaseAndPrint(n);
-  })
-  .catch(e => {
+process().then(() => {
+  console.log('작업이 끝났어요!');
+});
+
+async 함수에서 에러를 발생 시킬때에는 throw 를 사용하고, 에러를 잡아낼 때에는 try/catch 문을 사용합니다.
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function makeError() {
+  await sleep(1000);
+  const error = new Error();
+  throw error;
+}
+
+async function process() {
+  try {
+    await makeError();
+  } catch (e) {
     console.error(e);
-  });
-
-
-위 코드는 이렇게 정리를 할 수 있습니다.
-
-function increaseAndPrint(n) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const value = n + 1;
-      if (value === 5) {
-        const error = new Error();
-        error.name = 'ValueIsFiveError';
-        reject(error);
-        return;
-      }
-      console.log(value);
-      resolve(value);
-    }, 1000);
-  });
+  }
 }
 
-increaseAndPrint(0)
-  .then(increaseAndPrint)
-  .then(increaseAndPrint)
-  .then(increaseAndPrint)
-  .then(increaseAndPrint)
-  .then(increaseAndPrint)
-  .catch(e => {
-    console.error(e);
-  });
-Promise 를 사용하면, 비동기 작업의 개수가 많아져도 코드의 깊이가 깊어지지 않게 됩니다.
-
-하지만, 이것도 불편한점이 있긴 합니다. 
-에러를 잡을 때 몇번째에서 발생했는지 알아내기도 어렵고
- 특정 조건에 따라 분기를 나누는 작업도 어렵고, 
-특정 값을 공유해가면서 작업을 처리하기도 까다롭습니다. 
+process();
 
 
-다음 섹션에서 배울 async/await 을 사용하면, 이러한 문제점을 깔끔하게 해결 할 수 있습니다.
+이번에는, 비동기 함수를 몇개 더 만들어보겠습니다.
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const getDog = async () => {
+  await sleep(1000);
+  return '멍멍이';
+};
+
+const getRabbit = async () => {
+  await sleep(500);
+  return '토끼';
+};
+const getTurtle = async () => {
+  await sleep(3000);
+  return '거북이';
+};
+
+async function process() {
+  const dog = await getDog();
+  console.log(dog);
+  const rabbit = await getRabbit();
+  console.log(rabbit);
+  const turtle = await getTurtle();
+  console.log(turtle);
+}
+
+process();
+현재 위 코드에서는 getDog 는 1초, getRabbit 은 0.5초, getTurtle 은 3초가 걸리고 있습니다.
+ 이 함수들을 process 함수에서 연달아서 사용하게 되면서, process 함수가 실행되는 총 시간은 4.5초가 됩니다.
+
+지금은 getDog -> getRabbit -> getTurtle 순서대로 실행이 되고 있는데요, 
+하나가 끝나야 다음 작업이 시작하고 있는데, 동시에 작업을 시작하고 싶다면, 
+
+다음과 같이 Promise.all 을 사용해야합니다.
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const getDog = async () => {
+  await sleep(1000);
+  return '멍멍이';
+};
+
+const getRabbit = async () => {
+  await sleep(500);
+  return '토끼';
+};
+const getTurtle = async () => {
+  await sleep(3000);
+  return '거북이';
+};
+
+async function process() {
+  const results = await Promise.all([getDog(), getRabbit(), getTurtle()]);
+  console.log(results);
+}
+
+process();
+
+
+만약에 여기서 배열 비구조화 할당 문법을 사용한다면 각 결과값을 따로 따로 추출해서 조회 할 수 있습니다.
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const getDog = async () => {
+  await sleep(1000);
+  return '멍멍이';
+};
+
+const getRabbit = async () => {
+  await sleep(500);
+  return '토끼';
+};
+const getTurtle = async () => {
+  await sleep(3000);
+  return '거북이';
+};
+
+async function process() {
+  const [dog, rabbit, turtle] = await Promise.all([
+    getDog(),
+    getRabbit(),
+    getTurtle()
+  ]);
+  console.log(dog);
+  console.log(rabbit);
+  console.log(turtle);
+}
+
+process();
+Promise.all 를 사용 할 때에는, 등록한 프로미스 중 하나라도 실패하면, 모든게 실패 한 것으로 간주합니다.
+
+이번에는 Promise.race 라는 것에 대해서 알아봅시다. 
+
+이 함수는 Promise.all 과 달리, 여러개의 프로미스를 등록해서 실행했을 때 가장 빨리 끝난것 하나만의 결과값을 가져옵니다.
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const getDog = async () => {
+  await sleep(1000);
+  return '멍멍이';
+};
+
+const getRabbit = async () => {
+  await sleep(500);
+  return '토끼';
+};
+const getTurtle = async () => {
+  await sleep(3000);
+  return '거북이';
+};
+
+async function process() {
+  const first = await Promise.race([
+    getDog(),
+    getRabbit(),
+    getTurtle()
+  ]);
+  console.log(first);
+}
+
+process();
+Promise.race 의 경우엔 가장 다른 Promise 가 먼저 성공하기 전에 가장 먼저 끝난 Promise 가 실패하면 이를 실패로 간주합니다. 
+따라서, 현재 위의 코드에서 getRabbit 에서 에러를 발생시킨다면 에러를 잡아낼 수 있지만, 
+getTurtle 이나 getDog 에서 발생한 에러는 무시됩니다.
