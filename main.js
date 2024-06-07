e88d971b1cd68c45d6f5609cbfd3318fa7b410a2
kchore42 <155278187+kchore42@users.noreply.github.com>
2024-06-07 10:40:40 +0900

    JavaScript 기본 출력 console.log

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
-01. Hello JavaScript!
-자바스크립트는 여러분의 브라우저에서 언제든지 사용 할 수 있습니다. 만약 현재 여러분이 크롬브라우저가 아닌 다른 브라우저를 사용중이라면 크롬을 설치하여 실행해보세요. 그리고 개발자 도구를 열어보세요.
+console.log(1+2+3+4);
 
-개발자 도구는 윈도우 Ctrl + Shift + I 또는 macOS Command + Option + I 키를 눌러서 열 수 있습니다.
-
-크롬 개발자 도구에서 Console 탭을 열고 입력창에 다음 코드를 입력해보세요.
-
-console.log('Hello JavaScript!');
-
-여기서 console.log 는 콘솔에 특정 내용을 출력하라는 것을 의미합니다.
-
-
-console.log(1 + 2 + 3 + 4);
-
-console.log('안녕하세요!');
-console.log('JavaScript 를 배워봅시다'
+console.log('안녕하세요!);
+console.log(;JavaScript를 배워봅시다);
