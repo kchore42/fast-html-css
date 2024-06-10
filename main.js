ea29025430b6e1939ee6181c7ba1cba25e711b1f
kchore42 <155278187+kchore42@users.noreply.github.com>
2024-06-10 18:00:56 +0900

    14. 객체 - 객체 안에 함수 넣기

--- main.js
+++ main.js
@@ -1,56 +1,36 @@
+객체 안에 함수 넣기
 
-객체 비구조화 할당
+객체 안에 함수를 넣을 수도 있습니다. 한번 다음 코드를 실행해보세요.
 
-print 함수를 보시면 파라미터로 받아온 hero 내부의 값을 조회 할 때 마다 hero. 를 입력하고 있는데, 
-
-객체 비구조화 할당이라는 문법을 사용하면 코드를 더욱 짧고 보기 좋게 작성 할 수 있습니다.
-
-이 문법은 "객체 구조 분해" 라고 불리기도 합니다.
-
-const ironMan = {
-  name: '토니 스타크',
-  actor: '로버트 다우니 주니어',
-  alias: '아이언맨'
+const dog = {
+  name: '멍멍이',
+  sound: '멍멍!',
+  say: function say() {
+    console.log(this.sound);
+  }
 };
 
-const captainAmerica = {
-  name: '스티븐 로저스',
-  actor: '크리스 에반스',
-  alias: '캡틴 아메리카'
+dog.say();
+결과는 다음과 같습니다.
+
+멍멍!
+함수가 객체안에 들어가게 되면, this 는 자신이 속해있는 객체를 가르키게 됩니다.
+
+함수를 선언 할 때에는 이름이 없어도 됩니다.
+
+const dog = {
+  name: '멍멍이',
+  sound: '멍멍!',
+  say: function() {
+    console.log(this.sound);
+  }
 };
 
-function print(hero) {
-  const { alias, name, actor } = hero;
-  const text = `${alias}(${name}) 역할을 맡은 배우는 ${actor} 입니다.`;
-  console.log(text);
-}
+dog.say();
+이전과 똑같이 작동 할 것입니다.
 
-print(ironMan);
-print(captainAmerica);
-똑같은 결과가 나타났나요?
+객체 안에 함수를 넣을 때, 화살표 함수로 선언한다면 제대로 작동하지 않습니다.
 
-const { alias, name, actor } = hero;
-이 코드가 객체에서 값들을 추출해서 새로운 상수로 선언해 주는 것 입니다.
 
-여기서 더 나아가면, 파라미터 단계에서 객체 비구조화 할당을 할 수도 있습니다.
 
-const ironMan = {
-  name: '토니 스타크',
-  actor: '로버트 다우니 주니어',
-  alias: '아이언맨'
-};
-
-const captainAmerica = {
-  name: '스티븐 로저스',
-  actor: '크리스 에반스',
-  alias: '캡틴 아메리카'
-};
-
-function print({ alias, name, actor }) {
-  const text = `${alias}(${name}) 역할을 맡은 배우는 ${actor} 입니다.`;
-  console.log(text);
-}
-
-print(ironMan);
-print(captainAmerica);
-ㄴㄹ
+이유는, function 으로 선언한 함수는 this 가 제대로 자신이 속한 객체를 가르키게 되는데, 화살표 함수는 그렇지 않기 때문입니다.
