ba5bc7605891f5be14ea2a7e8c3400e8d8e5341e
kchore42 <155278187+kchore42@users.noreply.github.com>
2024-06-11 11:29:58 +0900

    15. 객체 - Getter 와 Setter 함수

--- main.js
+++ main.js
@@ -1,36 +1,80 @@
-객체 안에 함수 넣기
+Getter 함수와 Setter 함수
 
-객체 안에 함수를 넣을 수도 있습니다. 한번 다음 코드를 실행해보세요.
+객체 안에 Getter 함수와 Setter 함수를 설정하는 방법을 알아봅시다. 
 
-const dog = {
-  name: '멍멍이',
-  sound: '멍멍!',
-  say: function say() {
-    console.log(this.sound);
+객체를 만들고 나면, 다음과 같이 객체안의 값을 수정 할 수도 있는데요,
+
+const numbers = {
+  a: 1,
+  b: 2
+};
+
+numbers.a = 5;
+console.log(numbers);
+
+
+Getter 함수와 Setter 함수를 사용하게 되면 특정 값을 바꾸려고 하거나, 특정 값을 조회하려고 할 때 우리가 원하는 코드를 실행 시킬 수 있습니다.
+
+다음 코드를 따라 적어보세요.
+
+const numbers = {
+  a: 1,
+  b: 2,
+  get sum() {
+    console.log('sum 함수가 실행됩니다!');
+    return this.a + this.b;
   }
 };
 
-dog.say();
-결과는 다음과 같습니다.
+console.log(numbers.sum);
+numbers.b = 5;
 
-멍멍!
-함수가 객체안에 들어가게 되면, this 는 자신이 속해있는 객체를 가르키게 됩니다.
+console.log(numbers.sum);
 
-함수를 선언 할 때에는 이름이 없어도 됩니다.
 
-const dog = {
-  name: '멍멍이',
-  sound: '멍멍!',
-  say: function() {
-    console.log(this.sound);
+우리는 numbers.sum() 을 한 것이 아니라 number.sum 을 조회했을 뿐인데, 함수가 실행되고 그 결과값이 출력되었습니다.
+
+이런식으로 Getter 함수는 특정 값을 조회 할 때 우리가 설정한 함수로 연산된 값을 반환합니다.
+
+이번에는 Setter 함수를 사용해봅시다.
+
+const numbers = {
+  _a: 1,
+  _b: 2,
+  sum: 3,
+  calculate() {
+    console.log('calculate');
+    this.sum = this._a + this._b;
+  },
+  get a() {
+    return this._a;
+  },
+  get b() {
+    return this._b;
+  },
+  set a(value) {
+    console.log('a가 바뀝니다.');
+    this._a = value;
+    this.calculate();
+  },
+  set b(value) {
+    console.log('b가 바뀝니다.');
+    this._b = value;
+    this.calculate();
   }
 };
 
-dog.say();
-이전과 똑같이 작동 할 것입니다.
+console.log(numbers.sum);
+numbers.a = 5;
+numbers.b = 7;
+numbers.a = 9;
+console.log(numbers.sum);
+console.log(numbers.sum);
+console.log(numbers.sum);
 
-객체 안에 함수를 넣을 때, 화살표 함수로 선언한다면 제대로 작동하지 않습니다.
 
+Setter 함수를 설정 할 때에는 함수의 앞부분에 set 키워드를 붙입니다.
 
+Setter 함수를 설정하고 나면, numbers.a = 5 이렇게 값을 설정했을 때 5 를 함수의 파라미터로 받아오게 됩니다. 위 코드에서는 객체 안에 _a, _b 라는 숫자를 선언해주고, 이 값들을 위한 Getter 와 Setter 함수를 설정해주었습니다.
 
-이유는, function 으로 선언한 함수는 this 가 제대로 자신이 속한 객체를 가르키게 되는데, 화살표 함수는 그렇지 않기 때문입니다.
+아까 전에는 만든 객체에서는 numbers.sum 이 조회 될 때마다 덧셈이 이루어졌었지만, 이제는 a 혹은 b 값이 바뀔 때마다 sum 값을 연산합니다.
