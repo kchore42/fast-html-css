06b72408c208217fc15561eeeb3676e60db2d346
kchore42 <155278187+kchore42@users.noreply.github.com>
2024-06-11 14:28:09 +0900

    20. 반복문 - continue 와 break + 연습과 퀴즈

--- main.js
+++ main.js
@@ -1,45 +1,62 @@
-for...of
+break 와 continue
 
-for...of문은 배열에 관한 반복문을 돌리기 위해서 만들어진 반복문입니다.
+반복문 안에선 break와 continue를 통하여 반복분에서 벗어나거나, 그 다음 루프를 돌게끔 할 수 있다.
 
-사실 이 구문은 배워놔도 사용 할 일이 별로 없습니다. 보통 배열을 반복할때에는 배열의 내장함수를 많이 사용합니다. 
+for (let i = 0; i < 10; I++) {
+ if(i===2) continue; // 다음 루프를 실행
+ console.log(i);
+ if (i===5) break; // 반복문을 끝내기
+}
+i가 2일 땐 continue를 하여 언래 console를 해야 하지만 그 코드를 수행하지 않고 바로 3으로 넘어갑니다.
 
-그래도 알아는 둡시다.
+i가 5일 땐 break를 하여 반복문을 종료시킵니다.
 
-let numbers = [10, 20, 30, 40, 50];
-for (let number of numbers) {
-  console.log(number);
+function sum Of(numbers) {
+ let sum = 0;
+ for (let i = 0; i < numbers.length; i++) { 
+  sum +=numbers[i];
+  }
+ return sum;
 }
 
+const result = sumOf([1, 2, 3, 4, 5]);
+==================================
+퀴즈
+숫자로 이루어진 배열이 주어졌을 때, 해당 숫자 배열안에 들어있는 숫자 중 3보다 큰 숫자로만 이루어진 배열을 새로 만들어서 반환해보세요.
 
-객체를 위한 반복문 for...in
-
-객체를 위한 반복문을 알아보기 전에, 객체의 정보를 배열 형태로 받아올 수 있는 함수 몇가지를 알아보겠습니다.
-
-const doggy = {
-  name: '멍멍이',
-  sound: '멍멍',
-  age: 2
-};
-
-console.log(Object.entries(doggy));
-console.log(Object.keys(doggy));
-console.log(Object.values(doggy));
-
-
-각 함수의 역할은 다음과 같습니다.
-
-Object.entries: [[키, 값], [키, 값]] 형태의 배열로 변환
-Object.keys: [키, 키, 키] 형태의 배열로 변환
-Object.values: [값, 값, 값] 형태의 배열로 변환
-객체가 지니고 있는 값에 대하여 반복을 하고 싶다면 위 함수들을 사용하셔도 되고, for...in 구문을 사용하셔도 됩니다.
-
-const doggy = {
-  name: '멍멍이',
-  sound: '멍멍',
-  age: 2
-};
-
-for (let key in doggy) {
-  console.log(`${key}: ${doggy[key]}`);
+function biggerThanThree(numbers) {
+  /* 구현해보세요 */
 }
+======================================
+①
+function biggerThanThree(numbers) {
+const a =[];
+for (let = i 0; i , numbers.length; i++) {
+if( i < 3 ) continue;
+a.push(number[i]);
+}
+return a;
+
+② 
+function biggerThanThree(numbers) {
+numbers = numbers.filter((number) => number >3);
+return number;
+}
+
+const numbers2 = [1, 2, 3, 4, 5, 6, 7];
+console.log(biggerThanThree(numbers2)); // [4, 5, 6, 7]
+
+③
+function biggerThanThree(numbers) {
+k = []
+for (let number of number) {
+if(number >3) {
+k.push(number)
+}
+}
+
+return k
+}
+
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+console.log(biggerThanThree(numbers2)); // [4, 5, 6, 7]
