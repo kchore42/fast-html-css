5de2acacb11760b8c6413e57e7282c90f8631c29
kchore42 <155278187+kchore42@users.noreply.github.com>
2024-06-11 17:09:27 +0900

    26. 배열 내장함수 - shift, pop, unshift, push, concat, join

--- main.js
+++ main.js
@@ -1,26 +1,64 @@
-splice
+shift 와 pop
+shift 와 pop 은 비슷하지만, 다릅니다.
 
-splice는 배열에서 특정 항목을 제거할 때 사용한다.
-
-const numbers = [10, 20, 30, 40];
-위 배열에서 30을 지운다고 가정 
+shift는 첫번째 원소를 배열에서 추출(추출하는 과정에서 해당 원소는 사라진다.)
 
 const numbers = [10, 20, 30, 40];
-const index = numbers.indexOf(30);
-numbers.splice(index, 1);
-console.log(numbers)
+const value = numbers.shift();
+console.log(value);
+console.lof(numbers);
 
-splice를 사용할 때 첫번째 파라미터는 어떤 인덱스를 지울지를 의미
-두번째 파라미터는 그 인덱스부터 몇개를 지울지 의미
+결과 값
+[20, 30 ,40]
+
+const numbers = [10, 20, 30, 40];
+const value = numbers.pop();
+console.log(value);
+console.lof(numbers);
+
+결과 값
+[10, 20, 30]
+
+pop은 push의 반대로 생각하면 된다.
+
+push는 배열의 맨 마지막에 새 항복을 추가
+pop는 맨 마직막 항목 추출
 
 ==========================
-slice
-배열을 잘라낼 때 사용, 중요한 것은 기존의 배열을 건드리지 않는다는 것
+
+unshift
+unshift는 shift의 반대
+
+배열릐 맨 앞에 새 원소를 추가
 
 const numbers = [10, 20, 30, 40];
-const sliced = numbers.slice(0,2); // 0부터 시작해서 2전까지
+const value = numbers.unshift(5);
+console.log(value);
+console.lof(numbers);
 
-console.log(sliced); /[10, 20]
-console.log(numbers); //[10, 20, 30, 40]
+[5, 10, 20, 30, 40]
+================================================
+concat
 
-slice에는 두개의 파라미터를 넣게 되는데 첫번째 파라미터는 어디서부터 자를지, 그리고 두번째 파라미터는 어디까지 자를지를 의미
+concat은 여러개의 배열을 하나의 배열로 합쳐준다.
+
+const arr1 =[1, 2, 3];
+const arr2 =[4, 5, 6];
+const concated = arr1.concat(arr2);
+
+console.log(concated);
+
+결과
+[1, 2, 3, 4, 5, 6];
+
+concat 함수는 arr1과 arr2에 영향을 주지 않는다.
+
+================================
+
+join
+join 은 배열 안의 값들을 문자열 형태로 합쳐준다.
+
+const array = [1, 2, 3, 4, 5]
+console.log(array.join()); //1,2,3,4,5
+console.log(array.join( )); //1, 2, 3, 4 ,5
+console.log(array.join(', ')); //1, 2, 3, 4, 5
