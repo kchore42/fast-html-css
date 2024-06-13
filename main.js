4f97aae3b3bdc43f2dd9fc8ceacced7075bb0aba
kchore42 <155278187+kchore42@users.noreply.github.com>
2024-06-13 18:27:37 +0900

    30. 프로토타입과 클래스 - 객체 생성자

--- main.js
+++ main.js
@@ -1,115 +1,47 @@
-숫자 배열이 주어졌을 때 10보다 큰 숫자의 갯수를 반환하는 함수를 만드세요.
+프로토 타입과 클래스
 
-=======================================
-// forEach
-function countBiggerThenTen(numbers){
-let cnt = 0
-numbers.forEach(n => {
-cnt += n>10 ? 1 : 0
-})
-return cnt
+//객체 생성자
+
+객체 생성자는 함수를 통해서 새로운 객체를 만들고 그 안에 넣고 싶은 값 혹은 함수들을 구현할 수 있게 해준다.
+
+function Animal(type, name, sound) {
+ this.type = type;
+ this.name = name;
+ this.sound = sound;
+ this.say =function() {
+  console.log(this.sound);
+ };
 }
 
-const count = countBiggerThenTen([1, 2, 3, 5, 10, 20, 30, 40, 50, 60])
-console.log(count)
+const dog = new Animlal('개', '멍멍이', '멍멍');
+const cat = new Animal('고양이, '야옹이', '야옹');
 
-// 출력 :
-5
-============================
-// for
-function countBiggerThenTen(numbers){
-let cnt = 0
-for(let i=0;i<numbers.length;i++) {
-cnt += numbers[i]>10 ? 1 : 0
-}
-return cnt
-}
+dog.say();
+cat.say();
 
-<<<<<<< HEAD
-print(ironMan);
-print(captainAmerica);
-test
-=======
-const count = countBiggerThenTen([1, 2, 3, 5, 10, 20, 30, 40, 50, 60])
-console.log(count)
-// 출력 :
-5
-==================================
-// map
-function countBiggerThenTen(numbers){
-let cnt = 0;
-numbers.map((n) => {
-cnt += n > 10 ? 1 : 0
-})
-return cnt
+객체 생성자를 사용할 때는 보통 함수의 이름을 대문자로 시작하고, 새로운 객체를 만들 때에는 new 키워드를 앞에 붙여줘야 한다.
+
+지금 위 코드를 보면, dog 가 가지고 있는 say 함수와 cat 이 가지고 있는 수행하는 코드가 똑같음에도 불구하고 객체가 생성 될때마다 함수도 새로 만들어져서 this.say로 설정이 되고 있습니다.
+
+같은 객체 생성자 함수를 사용하는 경우, 특정 함수 또는 값을 재사용 할 수 있는게 바로 프로토 타입니다.
+
+프로토 타입
+
+프로토 타입은 다음과 같이 객체 생성자 함수 아래에 .protptype.[원하는 키] = 코드를 입력하여 설정할 수 있다.
+
+function Animal(type, name, sound) {
+ this.type = type;
+ this.name = name;
+ this.sound = sound;
 }
 
-const count = countBiggerThenTen([1, 2, 3, 5, 10, 20, 30, 40, 50, 60])
-console.log(count)
-// 출력 :
-5
-================================
-// filter
-function countBiggerThenTen(numbers){
-let cnt = 0;
-numbers.filter((n) => {
-cnt += n > 10 ? 1 : 0
-})
-return cnt
-}
+Animal.protype.sharedValue = 1;
 
-const count = countBiggerThenTen([1, 2, 3, 5, 10, 20, 30, 40, 50, 60])
-console.log(count)
-// 출력 :
-5
-================================
-// reduce
-function countBiggerThenTen(numbers){
-let cnt = numbers.reduce((res, cval, idx, arr) => {
-return res += cval > 10 ? 1 : 0
-}, 0)
-return cnt
-}
+const dog = new Animal('개', '멍멍이', '멍멍')
+const cat = new Animal('고양이', '야옹이', '야옹')
 
-const count = countBiggerThenTen([1, 2, 3, 5, 10, 20, 30, 40, 50, 60])
-console.log(count)
-// 출력 :
-5
-===============================
-// sol 1
-const countBiggerThanTen = (numbers) => {
-  const largerThen10 = numbers.filter((nums) => nums > 10);
-  return largerThen10.length;
-};
+dog.say();
+cat.say();
 
-const count = countBiggerThanTen([1, 2, 3, 5, 10, 20, 30, 40, 50, 60]);
-console.log(count); // 5
-=================================================
-// sol 2
-
-const countBiggerThanTen = (numbers) => {
-  const index = numbers.indexOf(10);
-  const largerThen10 = numbers.splice(index, numbers.length - index);
-  largerThen10.shift();
-  return largerThen10.length;
-};
-
-const count = countBiggerThanTen([1, 2, 3, 5, 10, 20, 30, 40, 50, 60]);
-console.log(count); // 5
-=========================================
-function countBiggerThanTen(numbers) {
-let num = numbers.reduce((accumulate, current) => {
-if(current > 10)
-return ++accumulate;
-else
-return accumulate;
-}, 0);
-
-return num;
-}
-
-const count = countBiggerThanTen([1, 2, 3, 5, 10, 20, 30, 40, 50, 60]);
-console.log(count); // 5
-
-export default countBiggerThanTen;
->>>>>>> 3e7a74a85efe5b7df3d2bd2e4dc2b2d0934fa2a7
+console.log(dog.sharedValue);
+console.log(cat.sdharedValue);
