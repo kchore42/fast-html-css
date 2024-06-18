f767523013e6127ca39623d40a5808ce95eb12a3
kchore42 <155278187+kchore42@users.noreply.github.com>
2024-06-18 14:36:46 +0900

    Truthy와 Falsy (2)

--- main.js
+++ main.js
@@ -1,110 +1,75 @@
-알고 있으면 유용한 JS 문법
+-------------------------------------------------------------------------------------------------
 
-02. Truthy and Falsy
+02. Truthy and Falsy(2)
 
-이것은 자바스크립트 문법까지는 아니지만, 알아둬야 하는 개념입니다.
+undefined 와 null 은 Falsy 한 값이다. Fasly 한 값 앞에 느낌표 를 붙여주면 true로 전환된다.
 
-Truthy: true 같은 거
-Falst: false 같은 거
+console.log(!undefined);
+consolelog(!null);
 
-라고 이해하면 된다.
+Falsy 한 값은 이 외에도 몇개 더 있다.
 
-function print(person) {
- console.log(person.name);
+console.log(!undefined);
+console.log(!null);
+consolel.log(!0);
+console.log(!'');
+consolel.(!NaN);
+
+이 값은 모두 true가 됩니다.
+
+NAN(= Not A Number)의 의미를 가지고 있다.
+
+보통 NaN 은 문자열을 숫자로 변환하는 자바스크립트 기본 함수 parselnt 라는 함수를 사용하게 될 때 볼 수 있다.
+
+//parseInt() 는 문자열 타입의 매개변수를 정수로 리턴해주는 함수
+
+const num = parselnt('15', 10); // 10진수 15를 숫자로 변환겠다는 의미 
+console.log(num); // 10
+const notnum = parselnt)('야호~', 10);
+console.log(notnum); // NaN
+
+다시 본론으로 돌아와서, Falsy 한 값은 아까 나열한 다섯 가지입니다.
+
+console.log(!undefined);
+console.log(!null);
+console.log(!0);
+console.log(!'');
+console.log(!NaN);
+
+그리고, 그 외의 값은 모두! Truthy한 값입니다.
+
+console.log(!3);
+console.log(!'hello');
+consolel.log(!['array?']
+console.log(![])
+console.log(!{value: 1 });
+
+이번에는 아까와는 반대로 모든 값이 false가 된다.
+
+Truthty한 값과 Falsy 한 값은 if문에서도 사용할 수 있다.
+
+const value = { a: 1 };
+if (value) { 
+ console.log('value 가 Truthy 하네요.');
 }
 
-const person = {
- name: 'john'
-};
+value가 Truthy 한 값이기 때문에, 콘솔에 메시지가 출력될 것입니다. 
+반면,  value가 null, undefined, 0, '', NaN 중 하나라면, 나타나지 않을 것이다.
 
-print(person)
+그래서 이렇게. Truthy 한 값과 Falsy 한 값을 잘 알아놓으면 조건문을 작성할 때 편하다.
+-------------------------------------------------------------------------------------------------
 
-만약에 이러한 상황에서, 만약 print 함수가 다음과 같이 파라미터가 비어진 채로 실행되었다고 가정
+알아두면 유용한 팁 하나!!
 
-function print(person) {
- console.log(person.name);
-}
+만약에, 특정 값이 Truthy 한 값이라면, 그렇지 않다면 false로 값을 표현하는 것을 구현해보겠습니다
 
-const person = {
- name. 'john'
-};
+const.= value ={ :1};
 
-print();
-이 코드는 다음과 같은 에러를 생성
+const truthy = value ? true: falself;
 
-TypeError: Cannot read property 'name' of undefined
+우리가 이전에 배운 삼항연산자를 사용하면 쉽게 value 값의 존재 유무에 따라 쉽게 true 및 false
+로 전환이 가능하다.
 
-이러한 상황에서, 만약에 print 함수에서 object가 주어지지 않았다면, 문제가 있다 콘솔 출력?
-
-function print(person) {
- if (person === undefined) {
-  return; 
-}
- console.log(person.name);
-}
-
-const person = {
- name: 'John'
-};
-
-print();
-
-그런데 만약에 print 에 null 값이 파라미터로 전달되면 어떨까?
-
-function print(person) {
- if (person === undefined) {
-  console.log('person이 없네요');
-  return;
-}
- console.log(person.name);
-
-const person = null;
-print(person); 
-
-또 오류 발생하게 된다.
-
-TypeError: Cannot read property 'name' of null
-
-그러면 또 print 함수에 조건을 추가해줘야 한다.
-
-function print(person) {
- if (person === undefined || person === unll) {
-  console.log('person'이 없네요');
-  return:
-}
- console.log(person.name);
-}
- 
-conse person = unll;
-print(person);
-
-이것을 더 축약해서 작성 가능
-
-function print(person) {
- if (!person) {
- console.log('person'이 없네요');
- return;
- }
- console.log(person.name);
-}
-
-const person = null;
-print(person);
-
-이게 작동하는 이유는 , undefined 와 null 은 Falsey 한 값입니다.
-
-Falsy한 값 앞에 느낍표를 붙여주면 true로 전환된다.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const value = { a:1 };
+const truthy = !!value;
+!value 는 false가 되고, 여기에 !false 는 true가 되어서, 결과는 true가 된다.
