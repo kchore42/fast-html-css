915c5bb30a27e4d025324bb220ce3d135725c2e6
김현진 <kchore615@gmail.com>
2024-06-18 19:10:16 +0900

    07. spread 와 rest

--- main.js
+++ main.js
@@ -1,182 +1,222 @@
-06. 비구조화 할당 (구조분해) 문법
-이번에는 1장 섹션 6 에서도 배웠던 비구조화 할당 문법을 잘 활용하는 방법에 대해서 알아보겠습니다.
+07. spread 와 rest
+이번에는 ES6 에서 도입된 spread 와 rest 문법에 대해서 알아보겠습니다. 서로 완전히 다른 문법인데요, 은근히 좀 비슷합니다.
 
-이전에 배웠던것을 복습해보자면, 비구조화 할당 문법을 사용하면 다음과 같이 객체 안에 있는 값을 추출해서 변수 혹은 상수로 바로 선언해 줄 수있었죠?
+spread
+일단 spread 문법부터 알아봅시다. spread 라는 단어가 가지고 있는 의미는 펼치다, 퍼뜨리다 입니다. 이 문법을 사용하면, 객체 혹은 배열을 펼칠수있습니다.
 
-const object = { a: 1, b: 2 };
+예를 들어서 다음과 같은 객체들이 있다고 가정해봅시다.
 
-const { a, b } = object;
-
-console.log(a); // 1
-console.log(b); // 2
-그리고, 함수의 파라미터에서도 비구조화 할당을 할 수 있는것도 배웠습니다.
-
-const object = { a: 1, b: 2 };
-
-function print({ a, b }) {
-  console.log(a);
-  console.log(b);
-}
-
-print(object);
-그런데 여기서 만약 b 값이 주어지지 않았다고 가정해봅시다.
-
-const object = { a: 1 };
-
-function print({ a, b }) {
-  console.log(a);
-  console.log(b);
-}
-
-print(object);
-// 1
-// undefined
-두번째 출력에서 undefined가 나타날 것입니다.
-
-비구조화 할당시 기본값 설정
-이러한 상황에 b 값에 기본 값을 주고 싶다면 이렇게 해줄 수 있습니다.
-
-const object = { a: 1 };
-
-function print({ a, b = 2 }) {
-  console.log(a);
-  console.log(b);
-}
-
-print(object);
-// 1
-// 2
-이는 꼭 함수의 파라미터에서만 할 수 있는 것은 아닙니다.
-
-const object = { a: 1 };
-
-const { a, b = 2 } = object;
-
-console.log(a); // 1
-console.log(b); // 2
-비구조화 할당시 이름 바꾸기
-이번에는, 비구조화 할당을 하는 과정에서 선언 할 값의 이름을 바꾸는 방법을 알아보겠습니다.
-
-예를 들어서 다음과 같은 코드가 있다고 가정해봅시다.
-
-const animal = {
-  name: '멍멍이',
-  type: '개'
+const slime = {
+  name: '슬라임'
 };
 
-const nickname = animal.name;
-
-console.log(nickname); // 멍멍이
-위 코드에서는 animal.name 값을 nickname 값에 담고 있는데요, 이름이 같다면 그냥 우리가 이전에 배웠던 대로 비구조화 할당을 쓰면 되는데 지금은 이름이 서로 다릅니다.
-
-이러한 상황에서는 : 문자를 사용해서 이름을 바꿔줄 수 있습니다.
-
-const animal = {
-  name: '멍멍이',
-  type: '개'
+const cuteSlime = {
+  name: '슬라임',
+  attribute: 'cute'
 };
 
-const { name: nickname } = animal
-console.log(nickname);
-위 코드는 'animal 객체 안에 있는 name 을 nickname 이라고 선언하겠다.' 라는 의미입니다.
+const purpleCuteSlime = {
+  name: '슬라임',
+  attribute: 'cute',
+  color: 'purple'
+};
 
-배열 비구조화 할당
-비구조화 할당은 객체에만 할 수 있는 것이 아닙니다. 배열에서 할 수 있어요.
+console.log(slime);
+console.log(cuteSlime);
+console.log(purpleCuteSlime);
 
-예시 코드를 봅시다.
 
-const array = [1, 2];
-const [one, two] = array;
+이 코드에서는 먼저 slime 이라는 객체를 선언했습니다. 그리고 cuteSlime 이라는 객체를 만들었는데요, 기존에 선언한 slime 을 건들이지 않고 새로운 객체를 만들어서 slime 이 가지고 있는 값을 그대로 사용하였습니다.
+
+그 다음에는 purpleCuteSlime 이라는 객체를 만들었는데요, 이 객체는 cuteSlime 이 가지고 있는 속성을 그대로 사용하면서 추가적으로 color 가 추가되었습니다.
+
+위 코드에서의 핵심은, 기존의 것을 건들이지 않고, 새로운 객체를 만든다는 것 인데요, 이러한 상황에 사용 할 수 있는 유용한 문법이 spread 입니다.
+
+아까 코드는 spread 문법을 사용하면 다음과 같이 작성 할 수 있습니다.
+
+const slime = {
+  name: '슬라임'
+};
+
+const cuteSlime = {
+  ...slime,
+  attribute: 'cute'
+};
+
+const purpleCuteSlime = {
+  ...cuteSlime,
+  color: 'purple'
+};
+
+console.log(slime);
+console.log(cuteSlime);
+console.log(purpleCuteSlime);
+여기서 사용한 ... 문자가 바로 spread 연산자입니다.
+
+spread 연산자는 배열에서도 사용 할 수 있습니다.
+
+const animals = ['개', '고양이', '참새'];
+const anotherAnimals = [...animals, '비둘기'];
+console.log(animals);
+console.log(anotherAnimals);
+
+
+기존의 animals 는 건드리지 않으면서, 새로운 anotherAnimals 배열에 animals 가 가지고 있는 내용을 모두 집어넣고, '비둘기' 라는 항목을 추가적으로 넣었습니다.
+
+배열에서 spread 연산자를 여러번 사용 할 수도 있습니다.
+
+const numbers = [1, 2, 3, 4, 5];
+
+const spreadNumbers = [...numbers, 1000, ...numbers];
+console.log(spreadNumbers); // [1, 2, 3, 4, 5, 1000, 1, 2, 3, 4, 5]
+rest
+rest는 생김새는 spread 랑 비슷한데, 역할이 매우 다릅니다.
+
+rest는 객체, 배열, 그리고 함수의 파라미터에서 사용이 가능합니다.
+
+객체에서의 rest
+우선 객체에서의 예시를 알아볼까요?
+
+const purpleCuteSlime = {
+  name: '슬라임',
+  attribute: 'cute',
+  color: 'purple'
+};
+
+const { color, ...rest } = purpleCuteSlime;
+console.log(color);
+console.log(rest);
+
+
+rest 안에 name 값을 제외한 값이 들어있습니다.
+
+rest 는 객체와 배열에서 사용 할 때는 이렇게 비구조화 할당 문법과 함께 사용됩니다. 주로 사용 할때는 위와 같이 rest 라는 키워드를 사용하게 되는데요, 추출한 값의 이름이 꼭 rest 일 필요는 없습니다.
+
+const purpleCuteSlime = {
+  name: '슬라임',
+  attribute: 'cute',
+  color: 'purple'
+};
+
+const { color, ...cuteSlime } = purpleCuteSlime;
+console.log(color);
+console.log(cuteSlime);
+이렇게 해도 무방합니다.
+
+이어서, attribute 까지 없앤 새로운 객체를 만들고 싶다면 이렇게 해주면 되겠죠.
+
+const purpleCuteSlime = {
+  name: '슬라임',
+  attribute: 'cute',
+  color: 'purple'
+};
+
+const { color, ...cuteSlime } = purpleCuteSlime;
+console.log(color);
+console.log(cuteSlime);
+
+const { attribute, ...slime } = cuteSlime;
+console.log(attribute);
+console.log(slime);
+
+
+배열에서의 rest
+다음, 배열에서의 사용 예시를 알아봅시다.
+
+const numbers = [0, 1, 2, 3, 4, 5, 6];
+
+const [one, ...rest] = numbers;
 
 console.log(one);
-console.log(two);
-이 문법은 배열 안에 있는 원소를 다른 이름으로 새로 선언해주고 싶을 때 사용하면 매우 유용합니다.
+console.log(rest);
 
-객체 비구조화 할당과 마찬가지로, 기본값 지정이 가능합니다.
 
-const array = [1];
-const [one, two = 2] = array;
+배열 비구조화 할당을 통하여 원하는 값을 밖으로 꺼내고, 나머지 값을 rest 안에 넣었습니다.
 
-console.log(one);
-console.log(two);
-깊은 값 비구조화 할당
-객체의 깊숙한 곳에 들어있는 값을 꺼내는 방법을 알아봅시다.
+반면 이렇게 할 수는 없답니다.
 
-예를들어서 다음과 같은 객체가 있다고 가정해봅시다.
+const numbers = [0, 1, 2, 3, 4, 5, 6];
 
-const deepObject = {
-  state: {
-    information: {
-      name: 'velopert',
-      languages: ['korean', 'english', 'chinese']
-    }
-  },
-  value: 5
-};
-여기서, name, languages, value 값들을 밖으로 꺼내주고 싶다면 어떻게 해야 할까요? 이럴땐 두가지 해결 방법이 있는데요, 첫번째는 비구조화 할당 문법을 두번 사용하는 것입니다.
+const [..rest, last] = numbers;
 
-const deepObject = {
-  state: {
-    information: {
-      name: 'velopert',
-      languages: ['korean', 'english', 'chinese']
-    }
-  },
-  value: 5
-};
 
-const { name, languages } = deepObject.state.information;
-const { value } = deepObject;
+함수 파라미터에서의 rest
+rest 를 함수파라미터에서 사용 할 수도 있습니다. 예를 들어서 우리가 파라미터로 넣어준 모든 값들을 합해주는 함수를 만들어주고 싶다고 가정해봅시다.
 
-const extracted = {
-  name,
-  languages,
-  value
-};
-
-console.log(extracted); // {name: "velopert", languages: Array[3], value: 5}
-그런데 잠깐! 지금 extracted 객체를 선언 할 때 이런식으로 했는데요
-
-const extracted = {
-  name,
-  languages,
-  value
+function sum(a, b, c, d, e, f, g) {
+  let sum = 0;
+  if (a) sum += a;
+  if (b) sum += b;
+  if (c) sum += c;
+  if (d) sum += d;
+  if (e) sum += e;
+  if (f) sum += f;
+  if (g) sum += g;
+  return sum;
 }
-이 코드는 다음 코드와 똑같습니다.
 
-const extracted = {
-  name: name,
-  languages: languages,
-  value: value
+const result = sum(1, 2, 3, 4, 5, 6);
+console.log(result);
+위에서의 sum 함수는 7개의 파라미터를 받아오는데, 아래에서 사용 할때에는 6개만 넣어줬습니다. 그러면, g 값이 undefined 가 되기 때문에 sum 에 더하는 과정에서 += undefined 를 하게 되면 결과는 NaN 이 되버립니다. 그렇기 때문에 함수에서 하나하나 유효한 값인지 확인을 해줬지요.
+
+함수의 파라미터가 몇개가 될 지 모르는 상황에서 rest 파라미터를 사용하면 매우 유용합니다. 코드를 다음과 같이 수정해보세요.
+
+function sum(...rest) {
+  return rest;
 }
-만약에 key 이름으로 선언된 값이 존재하다면, 바로 매칭시켜주는 문법입니다. 이 문법은 ES6 의 object-shorthand 문법이라고 부릅니다. (이름은 굳이 알아둘 필요는 없습니다..!)
 
-다시 본론으로 돌아와서, 아까 deepObject 객체에서 names, languages, value 를 추출하는 과정에서 비구조화 할당을 두번 했었죠?
+const result = sum(1, 2, 3, 4, 5, 6);
+console.log(result);
 
-이번엔 두번째 방법, 한번에 모두 추출하는 방법을 알아보겠습니다.
 
-const deepObject = {
-  state: {
-    information: {
-      name: 'velopert',
-      languages: ['korean', 'english', 'chinese']
-    }
-  },
-  value: 5
-};
+result 가 가르키고 있는 것은 함수에서 받아온 파라미터들로 이루어진 배열입니다. 우리가 이제 파라미터들이 들어가있는 배열을 받았으니, 그냥 모두 더해주면 되겠죠?
 
-const {
-  state: {
-    information: { name, languages }
-  },
-  value
-} = deepObject;
+function sum(...rest) {
+  return rest.reduce((acc, current) => acc + current, 0);
+}
 
-const extracted = {
-  name,
-  languages,
-  value
-};
+const result = sum(1, 2, 3, 4, 5, 6);
+console.log(result); // 21
+여기서 reduce 함수가 사용 됐는데, reduce 를 잘 모르겠으면 1장 섹션 9를 복습하고 오세요.
 
-console.log(extracted);
-이렇게 하면 깊숙히 안에 들어있는 값도 객체에서 바로 추출 할 수 있답니다.
+함수 인자와 spread
+이번에는, 다시 아까 배웠던 spread 로 돌아와서 한가지를 더 가르쳐드리겠습니다. 바로 함수의 인자와 spread 인데요, 만약 프로그래밍을 처음 배우신다면 파라미터와 인자가 좀 헷갈릴 수 있습니다. 이에 대해서 간단하게 설명 드려볼게요.
+
+const myFunction(a) { // 여기서 a 는 파라미터
+  console.log(a); // 여기서 a 는 인자
+}
+
+myFunction('hello world'); // 여기서 'hello world' 는 인자
+함수에서 값을 읽을때, 그 값들은 파라미터라고 부릅니다. 그리고 함수에서 값을 넣어줄 때, 그 값들은 인자라고 부릅니다.
+
+인자가 무엇인지 이해를 하셨다면 이제 함수인자와 spread 문법을 사용하는 것에 대하여 알아볼게요.
+
+우리가 방금 함수파라미터와 rest 를 사용한 것과 비슷한데, 반대의 역할입니다. 예를 들어서, 우리가 배열 안에 있는 원소들을 모두 파라미터로 넣어주고 싶다고 가정해보겠습니다.
+
+function sum(...rest) {
+  return rest.reduce((acc, current) => acc + current, 0);
+}
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const result = sum(
+  numbers[0],
+  numbers[1],
+  numbers[2],
+  numbers[3],
+  numbers[4],
+  numbers[5]
+);
+console.log(result);
+굉장히 불편하죠? 만약에 sum함수를 사용 할 때 인자 부분에서 spread 를 사용하면 다음과 같이 표현이 가능합니다.
+
+function sum(...rest) {
+  return rest.reduce((acc, current) => acc + current, 0);
+}
+
+const numbers = [1, 2, 3, 4, 5, 6];
+const result = sum(...numbers);
+console.log(result);
+어떤가요? 정말 편하죠?
+
+이렇게, spread 와 rest 를 잘 사용하면 앞으로 보기 깔끔한 코드를 작성하는 것에 큰 도움을 줄 것입니다.
+
